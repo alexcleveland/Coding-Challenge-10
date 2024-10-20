8f72afe58789c583a9ccc2d08d3530fffcfc1212
Alex Cleveland <acleveland2@usf.edu>
2024-10-19 20:30:26 -0400

    Implement event delegation to handle dynamically added products. forgot to commit before fixes so they are here to

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 document.addEventListener(`DOMContentLoaded`,() => {
     const sizeSelector =document.getElementById(`size-selector`);
     const productPrice = document.getElementById(`product-price`);
-    const purchaseButton =document.getElementById(`Purchase-button`);
+    const purchaseButton =document.getElementById(`purchase-button`);
+    const addProductForm = document.getElementById('add-product-form');
+    const productList = document.getElementById('product-list');
 
     sizeSelector.addEventListener(`change`, () => {
         const selectedOption = sizeSelector.options[sizeSelector.selectedIndex];
-        const newPrice = selectedOption.getattribute(`data-price`);
-        const stockStatus = selectedOption.getattribute(`data-stock`);
+        const newPrice = selectedOption.getAttribute(`data-price`);
+        const stockStatus = selectedOption.getAttribute(`data-stock`);
 
-        productPrice.textContent=`$${newPrice}`;
+        productPrice.textContent= newPrice;
         if(stockStatus === `out-of-stock`){
             purchaseButton.disabled = true;
         }else{
             purchaseButton.disabled =false;
         }
-    });
+    
 });
 
 purchaseButton.addEventListener(`click`, () => {
     const selectedOption = sizeSelector.options[sizeSelector.selectedIndex];
-    const stockStatus = selectedOption.getattribute(`data-stock`);
+    const stockStatus = selectedOption.getAttribute(`data-stock`);
     if (stockStatus === `in-stock`) {
         alert(`Thank you for shopping with us!!!`);
     } else {
         alert(`Sorry for the inconvinece this is out of stock :(`);
     }
     
+});
+
+addProductForm.addEventListener(`submit`,(event) => {
+    event.preventDefault();
+
+    const name = document.getElementById(`new-product-name`).value;
+    const price = document.getElementById(`new-product-price`).value;
+    const size = document.getElementById(`new-product-size`).value;
+    const stock = document.getElementById(`new-product-stock`).value;
+
+    const newProductHTML =`
+    <div class= "product">
+        <h3>${name}</h3>
+        <p>Price: $${price}</p>
+        <label for= "${size}">Select Size: </label>
+        <select id="${size}">
+           <option value="${size}" data-price="${price}" data-stock="${stock.toLowerCase()}">${size}: ${stock}</option>
+        </select>
+    </div>   `;
+
+ productList.insertAdjacentHTML(`beforeend`, newProductHTML);
+ addProductForm.reset();
+});
 });
